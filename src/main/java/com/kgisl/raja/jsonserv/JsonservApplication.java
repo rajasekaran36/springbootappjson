f512a408cdf49db00356069b78c3eb0506203a74
Rajasekaran S <proffraja@gmail.com>
2020-03-31 20:33:38 +0530

    test

---
+++ src/main/java/com/kgisl/raja/jsonserv/JsonservApplication.java
@@ -0,0 +1,13 @@
+package com.kgisl.raja.jsonserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgisl/raja/jsonserv/JsonservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgisl.raja.jsonserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
